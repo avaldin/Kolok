60ec1f9f86ac3080d26768f20e61a033abb2bf55
avaldin <valdinalan.pro@gmail.com>
2025-04-09 11:32:44 +0200

    started the homepage.

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {cn} from "@/lib/utils";
+
+export default function Home() {
+  return (
+      <>
+          <main>
+              <header className=""></header>
+          </main>
+      </>
+  );
+}
