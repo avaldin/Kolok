9c772fa58c39f3c6894a5493841107f1d30b7c19
avaldin <valdinalan.pro@gmail.com>
2025-04-11 10:26:02 +0200

    feat: header component

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {cn} from "@/lib/utils";
+import {Header} from "@/components/layout/header"
 
 export default function Home() {
   return (
       <>
-          <main >
-              <header className="bg-bistre text-white px-6 py-8 shadow-md">
-
-              </header>
+          <main className="min-h-screen">
+              <Header />
           </main>
       </>
   );
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,14 @@
+
+function Header() {
+    return (
+        <header className="bg-bistre text-white px-6 py-4 shadow-2xl">
+            <div className="max-w-6xl mx-auto flex items-center">
+                <h1 className="text-3xl sm:text-4xl font-bold text-cadet-gray">
+                    KoloK
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export {Header}
