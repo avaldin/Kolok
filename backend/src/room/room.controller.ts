68923c72b020e19a87047a27b6c0d4b4c7a17c92
avaldin <valdinalan.pro@gmail.com>
2025-06-24 22:57:16 +0200

    feat: room look finished, i have to implement unit test and try to use it with an applicatio to request api.

--- backend/src/room/room.controller.ts
+++ backend/src/room/room.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { RoomService } from './room.service';
+import { NameValidationPipe } from './pipe/nameValidation.pipe';
 import { Room } from './room.entity';
 
 @Controller('room')
@@ -7,7 +8,9 @@ export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
   @Post()
-  async createRoom(@Body('name') name: string): Promise<Room> {
+  async createRoom(
+    @Body('name', NameValidationPipe) name: string,
+  ): Promise<Room> {
     return this.roomService.createRoom(name);
   }
 
@@ -17,7 +20,30 @@ export class RoomController {
   }
 
   @Get(':name')
-  async getRoomByName(@Param('name') name: string): Promise<Room | null> {
+  async getRoomByName(
+    @Param('name', NameValidationPipe) name: string,
+  ): Promise<Room> {
     return this.roomService.findByName(name);
+  }
+
+  @Post(`room/:name/participants`)
+  async addParticipant(
+    @Param('roomName', NameValidationPipe) roomName: string,
+    @Body('participantName', NameValidationPipe) participantName: string,
+  ) {
+    await this.roomService.addParticipant(roomName, participantName);
+  }
+
+  @Delete(':name/participants/:participantName')
+  async removeParticipant(
+    @Param('participantName', NameValidationPipe) participantName: string,
+    @Param('roomName', NameValidationPipe) roomName: string,
+  ) {
+    await this.roomService.removeParticipant(roomName, participantName);
+  }
+
+  @Get(':name/participants')
+  async getParticipants(@Param('name', NameValidationPipe) name: string) {
+    return this.roomService.findAll();
   }
 }
--- backend/src/room/room.service.ts
+++ backend/src/room/room.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Room } from './room.entity';
@@ -11,6 +15,9 @@ export class RoomService {
   ) {}
 
   async createRoom(name: string): Promise<Room> {
+    const existingRoom = await this.roomRepository.findOne({ where: { name } });
+    if (existingRoom)
+      throw new ConflictException(`room with name ${name} already exists`);
     const room = this.roomRepository.create({ name });
     return this.roomRepository.save(room);
   }
@@ -19,7 +26,35 @@ export class RoomService {
     return this.roomRepository.find();
   }
 
-  async findByName(name: string): Promise<Room | null> {
-    return this.roomRepository.findOne({ where: { name } });
+  async findByName(name: string): Promise<Room> {
+    const roomByName = await this.roomRepository.findOne({ where: { name } });
+    if (!roomByName)
+      throw new NotFoundException(`room with name ${name} doesn't exist`);
+    return roomByName;
+  }
+
+  async addParticipant(roomName: string, participantName: string) {
+    const room = await this.findByName(roomName);
+    if (room.participants.includes(participantName))
+      throw new ConflictException(
+        `${participantName} est deja dans cette room`,
+      );
+    room.participants.push(participantName);
+  }
+
+  async removeParticipant(
+    roomName: string,
+    participantName: string,
+  ): Promise<void> {
+    const room = await this.findByName(roomName);
+    if (!room.participants.includes(roomName))
+      throw new NotFoundException(
+        `${participantName} n'est pas dans cette room`,
+      );
+  }
+
+  async participantsByRoom(roomName: string): Promise<string[]> {
+    const room = await this.findByName(roomName);
+    return room.participants;
   }
 }
